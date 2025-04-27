c2047daa056ae442732c18a3314ad69400ee7d44
harispring <hari.spring4s@gmail.com>
2025-04-27 12:17:27 +0530

    changes were added

--- src/main/java/com/mailsender/springmailexample/SpringmailexampleApplication.java
+++ src/main/java/com/mailsender/springmailexample/SpringmailexampleApplication.java
@@ -11,7 +11,7 @@ public class SpringmailexampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmailexampleApplication.class, args);
 
-// changes are merged from hari to main
+// changes are merged from hari to main merged
 
 
 
